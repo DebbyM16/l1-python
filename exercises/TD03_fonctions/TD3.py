93f6cbfaaf5a67a4ddf84fb0e73f190a13c5a659
Déborah MOREL <debbymorel@yahoo.com>
2022-10-14 09:11:07 +0200

    TD3

--- exercises/TD03_fonctions/TD3.py
+++ exercises/TD03_fonctions/TD3.py
@@ -1,20 +1,31 @@
-#temps[0] : jours, temps[1]: heures, temps[2]: minutes, temps[3]: secondes
 
 def tempsEnSeconde(temps):
-    """ Renvoie la valeur en seconde de temps donné comme jour, heure, minute, seconde."""
-    conversion1=(temps[0]*24*3600)+(temps[1]*3600)+(temps[2]*60)+temps[3]
+    """ Renvoie la valeur en seconde de temps."""
+    jour = temps[0] * 24 * 3600
+    heure = temps[1] * 3600
+    minute = temps[2] * 60
+    seconde = temps[3]
+    conversion1 = jour + heure + minute + seconde
+    return conversion1
 
-temps = (3,23,1,34)
+
+temps = (3, 23, 1, 34)
 print(type(temps))
-print(tempsEnSeconde(temps))   
+print(tempsEnSeconde(temps), "secondes")
+
 
 def secondeEnTemps(seconde):
-    """Renvoie le temps (jour, heure, minute, seconde) qui correspond au nombre de seconde passé en argument"""
-    jours=seconde//(24*3600)
-    heures=(jours%(24*3600))//3600
-    minutes=(heures%3600)//60
-    secondes=minutes%60
-    conversion2=(jours,heures,minutes,secondes)
-    
+    """Renvoie le temps qui correspond au nombre de seconde."""
+    jours = seconde // (24 * 3600)
+    heures = (jours % (24 * 3600)) // 3600
+    minutes = (heures % 3600) // 60
+    secondes = minutes % 60
+    conversion2 = (jours, heures, minutes, secondes)
+    return conversion2
+
+
 temps = secondeEnTemps(100000)
-print(temps[0],"jours",temps[1],"heures",temps[2],"minutes",temps[3],"secondes")
+print(temps[0], "jours")
+print(temps[1], "heures")
+print(temps[2], "minutes")
+print(temps[3], "secondes")
