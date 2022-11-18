f4ca0b87b8a742d599c4251ad369c947ff4a379c
Déborah MOREL <debbymorel@yahoo.com>
2022-11-18 09:54:15 +0100

    TD4

--- exercises/TD04_listes/syracuse.py
+++ exercises/TD04_listes/syracuse.py
@@ -1,14 +1,32 @@
+# problème de Syracuse
+
 def syracuse(n):
     """ Retourne la liste des valeurs de la suite en partant de n jusqu'à 1 """
-    L = [n]
+    L_syracuse = [n]
     while n != 1:
         if n % 2 == 0:
             n = n//2
-            L.append(n)
+            L_syracuse.append(n)
         else:
             n = (n * 3) + 1
-            L.append(n)
-    return (L)
+            L_syracuse.append(n)
+    return L_syracuse
 
 
 print(syracuse(3))
+
+# Conjecture de Collatz
+
+
+def testeConjecture(n_max):
+    """ Teste la conjecture de Collatz pour toutes les valeurs de 1 à n_max """
+    L_collatz = syracuse(n_max)
+    if L_collatz[-1] == 1:
+        collatz = True
+    else:
+        collatz = False
+    return collatz
+
+
+print(testeConjecture(10000))
+
